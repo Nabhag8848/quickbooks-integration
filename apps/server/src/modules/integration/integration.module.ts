178ee10791454cdf571b54b1c7ab8c43b26f99c3
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2026-01-18 12:53:00 +0530

    setup bullmq

--- apps/server/src/modules/integration/integration.module.ts
+++ apps/server/src/modules/integration/integration.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { QuickbooksOnlineModule } from './quickbooks-online/quickbooks-online.module';
 import { OAuthModule } from './oauth/oauth.module';
 import { SyncModule } from './sync/sync.module';
+import { QueueModule } from '@/modules/queue/queue.module';
+
 @Module({
-  imports: [QuickbooksOnlineModule, OAuthModule, SyncModule],
-  exports: [QuickbooksOnlineModule, OAuthModule, SyncModule],
+  imports: [QueueModule, QuickbooksOnlineModule, OAuthModule, SyncModule],
+  exports: [QueueModule, QuickbooksOnlineModule, OAuthModule, SyncModule],
 })
 export class IntegrationModule {}
--- apps/server/src/modules/modules.module.ts
+++ apps/server/src/modules/modules.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { IntegrationModule } from './integration/integration.module';
 import { CompanyModule } from './company/company.module';
 import { ApiModule } from './api/api.module';
+import { QueueModule } from './queue/queue.module';
 
 @Module({
-  imports: [CompanyModule, IntegrationModule, ApiModule],
-  exports: [IntegrationModule, CompanyModule, ApiModule],
+  imports: [CompanyModule, IntegrationModule, ApiModule, QueueModule],
+  exports: [IntegrationModule, CompanyModule, ApiModule, QueueModule],
 })
 export class ModulesModule {}
